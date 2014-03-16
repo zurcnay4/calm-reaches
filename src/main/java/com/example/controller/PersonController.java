ab362edc609bd5afe869a9ba3b57b83ef9222b64
zurcnay4 <zurcnay4@gmail.com>
2014-03-16 23:17:51 +0800

    change

--- src/main/java/com/example/controller/PersonController.java
+++ src/main/java/com/example/controller/PersonController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import com.example.model.Person;
 import com.example.service.PersonService;
 
+import java.util.List;
 import java.util.Map;
 
 @Controller
@@ -16,14 +17,11 @@ public class PersonController {
     @Autowired
     private PersonService personService;
 
-    @RequestMapping("/")
-    public String listPeople(Map<String, Object> map) {
-
-        map.put("person", new Person());
-        map.put("peopleList", personService.listPeople());
-
-        return "people";
+    @RequestMapping("/rest")
+    public @ResponseBody List<Person> listPeople() {
+        return personService.listPeople();
     }
+/*
 
     @RequestMapping(value = "/add", method = RequestMethod.POST)
     public String addPerson(@ModelAttribute("person") Person person, BindingResult result) {
@@ -40,13 +38,14 @@ public class PersonController {
 
         return "redirect:/people/";
     }
+*/
 
-    @RequestMapping(value="/{name}", method = RequestMethod.GET)
+   /* @RequestMapping(value="/{name}", method = RequestMethod.GET)
     public @ResponseBody Person getPersonInJSON(@PathVariable String name) {
         Person p = new Person();
         p.setFirstName(name);
         p.setLastName("JSON");
 
         return p;
-    }
+    }*/
 }
--- src/main/java/com/example/controller/PersonUIController.java
+++ src/main/java/com/example/controller/PersonUIController.java
@@ -1,7 +1,30 @@
 package com.example.controller;
 
+import com.example.model.Person;
+import com.example.service.PersonService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+import java.util.Map;
+
 /**
  * Created by ayancruz on 3/16/14.
  */
+@Controller
+@RequestMapping(value="/people")
 public class PersonUIController {
+
+    @Autowired
+    private PersonService personService;
+
+    public String getPeoplePage(Model model) {
+        model.addAttribute("peopleList", personService.listPeople());
+
+        return "people";
+    }
 }
